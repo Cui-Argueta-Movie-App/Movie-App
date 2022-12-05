2fa547be041a1d9d0112ed1b678378905a5d39ef
Qiyuan Cui <cuiqiyuan1989@gmail.com>
2022-12-05 11:59:34 -0600

    var DOM in JS

---
+++ main.js
@@ -0,0 +1,22 @@
+//call DOM
+let homeIcon = document.getElementById("home");
+let searchInput = document.getElementById("search-input");
+let searchBtn = document.getElementById("search-btn");
+let loading = document.getElementById("loading");
+let movieInfo = document.getElementById('movie-info');
+let movieSelection = document.getElementById('movie-selection');
+
+//call DOM API
+
+let pic = document.querySelectorAll('.pic');
+let title = document.querySelectorAll('.title');
+let year = document.querySelectorAll('.year');
+let rating = document.querySelectorAll('.rating');
+let genre = document.querySelectorAll('.genre');
+let director = document.querySelectorAll('.director');
+let plot = document.querySelectorAll('.plot');
+
+//OMDb API
+
+
+
